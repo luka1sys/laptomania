7138619f52e6f18cbfd469b301596494eb0a8833
luka1sys <dgebuadzeluka2008@gmail.com>
2025-11-25 20:33:13 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const cookieParser = require('cookie-parser');
 // cybersecurity
 //იცავს API-ს ბევრი მოთხოვნებისგან
 // განსაზღვრავს თუ რამდენ მოთხოვნას შეუძლია მომხმარებელი გააკეთოს განსაზღვრულ დროში.
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 
 // // იცავს MongoDB-ს ჰაკერული payload-ებისგან (მაგ. $gt, $set, $push).
 // const mongooseSanitize = require('express-mongo-sanitize');
 // // აყენებს უსაფრთხოების HTTP headers, რაც იცავს XSS და სხვა რისკებისგან ბაზას.
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 // Rate Limiter Middleware
 dotenv.config();
@@ -31,14 +31,14 @@ app.use(cors({
 app.use(cookieParser());
 // app.use(mongooseSanitize());
 app.use(express.json());
-app.use(helmet())
-app.use(
-    rateLimit({
-        windowMs: 2 * 60 * 1000, // 15 minutes
-        max: 200, // limit each IP to 100 requests per windowMs
-        message: 'Too many requests from this IP, please try again later.'
-    })
-);
+// app.use(helmet())
+// app.use(
+//     rateLimit({
+//         windowMs: 2 * 60 * 1000, // 15 minutes
+//         max: 200, // limit each IP to 100 requests per windowMs
+//         message: 'Too many requests from this IP, please try again later.'
+//     })
+// );
 
 
 
