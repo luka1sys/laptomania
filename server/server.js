fcad830dc2d61a987d8954634e6344b93c13a792
luka1sys <dgebuadzeluka2008@gmail.com>
2025-11-25 19:24:54 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cors({
 // cybersecurity - rate limiter
 app.use(
     rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
+        windowMs: 2 * 60 * 1000, // 15 minutes
         max: 5, // limit each IP to 100 requests per windowMs
         message: 'Too many requests from this IP, please try again later.'
     })
